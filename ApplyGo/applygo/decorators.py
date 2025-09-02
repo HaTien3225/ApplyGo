76eedda2600e44c8ba7f9fb843df68fae568764b
Loc sua bai <tranquangloc2004@gmail.com>
2025-09-02 19:36:52 +0700

    AG-31

--- ApplyGo/applygo/decorators.py
+++ ApplyGo/applygo/decorators.py
@@ -1,8 +1,6 @@
 from functools import wraps
-from flask import abort
-
 from flask import request, redirect, url_for
-from flask_login import current_user, login_required
+from flask_login import current_user
 
 
 def loggedin(f):
@@ -14,14 +12,3 @@ def loggedin(f):
         return f(*args, **kwargs)
 
     return decorated_function
-
-def role_required(*roles):
-    def decorator(f):
-        @wraps(f)
-        @login_required  # đảm bảo user đã login
-        def decorated_function(*args, **kwargs):
-            if current_user.role not in roles:
-                abort(403)  # trả 403 Forbidden nếu không đủ quyền
-            return f(*args, **kwargs)
-        return decorated_function
-    return decorator
